703a3c0e210574f4bc56d864db43f44a5612fcfa
dianannperla <waafirmelyza@hotmail.com>
2025-09-05 19:29:46 +0100

    feat: initial commit for src/index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './style.css';
+
+const app = document.getElementById('app');
+
+// Khởi tạo ứng dụng
+const initApp = () => {
+    app.innerHTML = '<h1>Chào mừng đến với Front End Framework Vietnam!</h1>';
+};
+
+initApp();
